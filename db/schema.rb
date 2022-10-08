298631af9d40df1c8e66bf81798648a5bfd54ea6
kobassann <108574108+kobassann@users.noreply.github.com>
2022-10-08 18:54:15 +0000

    モデル名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_035801) do
+ActiveRecord::Schema.define(version: 2022_10_08_183739) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2022_10_08_035801) do
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
 end
