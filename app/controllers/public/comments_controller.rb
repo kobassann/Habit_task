4c74ddc9e33044d9dd9f95d06db74c3ae64bcddd
kobassann <108574108+kobassann@users.noreply.github.com>
2022-10-24 20:41:52 +0000

    レイアウト調整中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,20 +1,22 @@
 class Public::CommentsController < ApplicationController
   def create
     @task = Task.find(params[:task_id])
-    @comment = current_member.comments.new(comment_params.merge(task_id: params[:task_id]))
+    @comment = current_member.comments.new(comment_params)#.merge(task_id: params[:task_id]))
+    # @comment.task_id = params[:task_id]
     @comment.save
     # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @task = Task.find(params[:task_id])
-    @comment = Comment.find_by(id: params[:id], task_id: params[:task_id])
+    # @comment = Comment.find_by(id: params[:id], task_id: params[:task_id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
     # redirect_back(fallback_location: root_path)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment_text)
+    params.require(:comment).permit(:comment_text, :task_id)
   end
 end
